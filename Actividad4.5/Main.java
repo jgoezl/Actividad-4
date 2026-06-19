6b4d613e60f2b22d7403ff9ec7593c62ee96583d
jgoezl <jgoezl@unal.edu.co>
2026-06-18 20:48:25 -0500

    Create Main.java

---
+++ Actividad4.5/Main.java
@@ -0,0 +1,10 @@
+package actividad4.pkg5;
+public class Actividad45 {
+
+    public static void main(String[] args) {
+        Interfaz inter = new Interfaz();
+        inter.setVisible(true);
+        System.out.println(inter.getClass());
+    }
+    
+}
