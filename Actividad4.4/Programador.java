080d0912fc0422dd1bf7a30ab6779ac81874e35e
jgoezl <jgoezl@unal.edu.co>
2026-06-18 20:47:03 -0500

    Create Programador.java

---
+++ Actividad4.4/Programador.java
@@ -0,0 +1,21 @@
+
+package actividad4.pkg4;
+
+
+public class Programador {
+    String nombre;
+    String apellidos;
+
+    Programador(String nombre, String apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    String getNombre() {
+        return nombre;
+    }
+
+    String getApellidos() {
+        return apellidos;
+    }
+}
