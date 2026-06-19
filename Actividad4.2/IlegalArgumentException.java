97a7b8761e626ab755dce4154ed51f4925052caa
jgoezl <jgoezl@unal.edu.co>
2026-06-18 20:42:38 -0500

    Create IlegalArgumentException.java

---
+++ Actividad4.2/IlegalArgumentException.java
@@ -0,0 +1,9 @@
+package actividad4.pkg2;
+
+
+public class IlegalArgumentException extends Exception {
+
+    public IlegalArgumentException(String el_vendedor_debe_ser_mayor_de_18_años) {
+    }
+    
+}
