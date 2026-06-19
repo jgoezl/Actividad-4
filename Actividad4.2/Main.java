eea0900b2e890f4312aa73fd1ad3188e9452e256
jgoezl <jgoezl@unal.edu.co>
2026-06-18 20:41:50 -0500

    Create Main.java

---
+++ Actividad4.2/Main.java
@@ -0,0 +1,9 @@
+package actividad4.pkg2;
+
+public class Actividad42 {
+    public static void main(String[] args) {
+        VentanaVendedor v = new VentanaVendedor();
+        v.setVisible(true);
+    }
+    
+}
